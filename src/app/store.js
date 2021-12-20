e13d345511d1652087a3fd0e25402f1b7ab0c39f
Ramaswamy Narayanan <>
2021-12-20 18:29:42 +0530

    added rtk-query and configured store

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,15 @@ import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from './features/posts/postsSlice'
 import usersReducer from './features/users/usersSlice'
 import notificationReducer from './features/notifications/notificationSlice'
+import { apiSlice } from './features/api/apiSlice'
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
     notifications: notificationReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 })
